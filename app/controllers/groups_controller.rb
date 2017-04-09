2e559f97b6c3b06e62464290e5a3cf1bf24a01b0
zhuqi <ardentyouny>
2017-04-09 14:25:06 +0800

    add post and hook group/user relatiaon

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -53,8 +53,8 @@ class GroupsController < ApplicationController
   def find_group_and_check_permission
     @group = Group.find(params[:id])
 
-    if current_user != group_user
-      redict_to root_path ,alert :"you have no permssion"
+    if current_user != group.user
+      redirect_to root_path, alert: "you have no permssion"
     end
   end
 
